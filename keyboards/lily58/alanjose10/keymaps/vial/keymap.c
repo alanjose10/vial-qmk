52199503303672977c55cad867e2bffb71198604
Alan Jose <alanjose10@gmail.com>
2025-09-06 17:45:06 +0100

    oled updates

--- keyboards/lily58/alanjose10/keymaps/vial/keymap.c
+++ keyboards/lily58/alanjose10/keymaps/vial/keymap.c
@@ -33,22 +33,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | TAB  |   ;  |   ,  |   .  |   P  |   Y  |                    |   F  |   G  |   C  |   R  |   L  |      |
+ * |      |   ;  |   ,  |   .  |   P  |   Y  |                    |   F  |   G  |   C  |   R  |   L  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | CPS  |   A  |CTL,O |ALT,E |GUI,U |   I  |-------.    ,-------|   D  |GUI,H |ALT,T |CTL,N |   S  |      |
+ * |      |   A  |CTL,O |ALT,E |GUI,U |   I  |-------.    ,-------|   D  |GUI,H |ALT,T |CTL,N |   S  |      |
  * |------+------+------+------+------+------| MOUSE |    |  TBD  |------+------+------+------+------+------|
  * |      |   '  |   Q  |   J  |   K  |   X  |-------|    |-------|   B  |   M  |   W  |   V  |   Z  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |      | Esc  | LOWER| /SFT,Spc/       \SFT,Ent\  |RAISE |BackSP|      |
+ *                   | CPS  | TAB  | LOWER| /SFT,Spc/       \SFT,Ent\  |RAISE | BKSP | ESC  |
  *                   |      |      |      |/       /         \      \  |      |      |      |
  *                   `-------------------''-------'           '------''---------------------'
  */
 [_BASE] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                 _______, _______, _______, _______, _______, _______,
-  KC_TAB, KC_SCLN, KC_COMM, KC_DOT,  KC_P,   KC_Y,                     KC_F,    KC_G,    KC_C,    KC_R,    KC_L, _______,
-  KC_CAPS, KC_A, LCTL_T(KC_O), LALT_T(KC_E), LGUI_T(KC_U), KC_I,        KC_D, RGUI_T(KC_H), RALT_T(KC_T), RCTL_T(KC_N), KC_S, _______,
+  _______, KC_SCLN, KC_COMM, KC_DOT,  KC_P,   KC_Y,                     KC_F,    KC_G,    KC_C,    KC_R,    KC_L, _______,
+  _______, KC_A, LCTL_T(KC_O), LALT_T(KC_E), LGUI_T(KC_U), KC_I,        KC_D, RGUI_T(KC_H), RALT_T(KC_T), RCTL_T(KC_N), KC_S, _______,
   _______, KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X, _______,          _______,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z, _______,
-                      _______, KC_ESC, LOWER, RSFT_T(KC_SPC),         RSFT_T(KC_ENT),  RAISE,  KC_BSPC,  _______
+                      KC_CAPS, KC_TAB, LOWER, RSFT_T(KC_SPC),         RSFT_T(KC_ENT),  RAISE,  KC_BSPC,  KC_ESC
 ),
 
 /* LOWER
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------.    ,-------| Left | Down |  Up  |Right |      |      |
  * |------+------+------+------+------+------|   \/  |    |  \/   |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|   +  |   =  |      |      |      |      |
+ * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |  \/  |  \/  |  \/  | /  \/   /       \ \/   \  |  \/  |  \/  |  \/  |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -127,74 +127,20 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 #ifdef OLED_ENABLE
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  if (!is_keyboard_master())
-    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-  return rotation;
+    if (!is_keyboard_master())
+        return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+    return rotation;
 }
-
-static void render_logo(void) {
-	
-    static const char PROGMEM logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
-        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
-    };
-
-    oled_write_P(logo, false);
-}
-
-char keylog_str[24] = {};
-char keylogs_str[21] = {};
-int keylogs_str_idx = 0;
 
 char wpm[4];
 char layer_misc[7];
 
-const char code_to_name[60] = {
-    ' ', ' ', ' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f',
-    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-    'R', 'E', 'B', 'T', '_', '-', '=', '[', ']', '\\',
-    '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
 
-void set_keylog(uint16_t keycode, keyrecord_t *record) {
-  char name = ' ';
-  if (keycode < 60) {
-    name = code_to_name[keycode];
-  }
-
-  // update keylog
-  snprintf(keylog_str, sizeof(keylog_str), "%dx%d, k%2d : %c",
-           record->event.key.row, record->event.key.col,
-           keycode, name);
-
-  // update keylogs
-  if (keylogs_str_idx == sizeof(keylogs_str) - 1) {
-    keylogs_str_idx = 0;
-    for (int i = 0; i < sizeof(keylogs_str) - 1; i++) {
-      keylogs_str[i] = ' ';
-    }
-  }
-
-  keylogs_str[keylogs_str_idx] = name;
-  keylogs_str_idx++;
-}
-
-const char *read_keylog(void) {
-  return keylog_str;
-}
-
-const char *read_keylogs(void) {
-  return keylogs_str;
-}
-
-static void render_status(void) {
-    oled_write_P(PSTR("layer "), false);
+static void render_layer_status(void) {
 
     switch (get_highest_layer(layer_state)) {
         case _BASE:
-            oled_write_P(PSTR("BASE"), false);
+            oled_write_P(PSTR(" BASE "), false);
             break;
         case _LOWER:
             oled_write_P(PSTR("LOWER "), false);
@@ -217,14 +163,82 @@ static void render_status(void) {
     oled_write_P(PSTR(" wpm\n"), false);
 }
 
+
+
+#define KEYLOG_LEN 6
+char     keylog_str[KEYLOG_LEN] = {};
+uint8_t  keylogs_str_idx        = 0;
+uint16_t log_timer              = 0;
+
+const char code_to_name[60] = {
+    ' ', ' ', ' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f',
+    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+    'R', 'E', 'B', 'T', '_', '-', '=', '[', ']', '\\',
+    '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
+
+void add_keylog(uint16_t keycode) {
+    if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) || (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) {
+        keycode = keycode & 0xFF;
+    }
+
+    for (uint8_t i = KEYLOG_LEN - 1; i > 0; i--) {
+        keylog_str[i] = keylog_str[i - 1];
+    }
+    if (keycode < 60) {
+        keylog_str[0] = code_to_name[keycode];
+    }
+    keylog_str[KEYLOG_LEN - 1] = 0;
+
+    log_timer = timer_read();
+}
+
+void update_log(void) {
+    if (timer_elapsed(log_timer) > 750) {
+        add_keylog(0);
+    }
+}
+
+void render_keylogger_status(void) {
+    oled_write(keylog_str, false);
+}
+
+void render_qmk_logo(void) {
+    static const char PROGMEM qmk_logo[] = {
+        0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
+        0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
+        0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
+    // clang-format on
+
+    oled_write_P(qmk_logo, false);
+}
+
+
+
+
 bool oled_task_user(void) {
-  if (is_keyboard_master()) {
-    render_status();
-  } else {
-    render_logo();
-  }
+    update_log();
+    if (is_keyboard_master()) {
+        render_layer_status();
+        oled_write_P(PSTR("\n"), false);
+
+        render_keylogger_status();
+        oled_write_P(PSTR("\n"), false);
+
+    } else {
+        render_qmk_logo();
+    }
     return false;
 }
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        add_keylog(keycode);
+    }
+    return true;
+}
+
 #endif // OLED_ENABLE
+
 
